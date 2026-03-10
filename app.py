f16d224321f6b66efbcb7a3e75bfab7eefaac97a
suhanishende06-rgb <suhanishende06@gmail.com>
2026-03-09 20:47:06 -0700

    Simplify QR code generation in app.py
    Refactor QR code generation and remove unused download route.

--- app.py
+++ app.py
@@ -1,34 +1,21 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request
 import qrcode
-from PIL import Image
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET","POST"])
+@app.route("/", methods=["GET", "POST"])
 def index():
+    qr = None
+    
     if request.method == "POST":
-        data = request.form["data"]
+        data = request.form.get("data")
 
-        qr = qrcode.QRCode(
-            version=1,
-            box_size=10,
-            border=5
-        )
+        if data:
+            img = qrcode.make(data)
+            img.save("static/qrcode.png")
+            qr = "static/qrcode.png"
 
-        qr.add_data(data)
-        qr.make(fit=True)
-
-        img = qr.make_image(fill_color="black", back_color="white")
-
-        img.save("static/qrcode.png")
-
-        return render_template("index.html", qr="static/qrcode.png")
-
-    return render_template("index.html")
-
-@app.route("/download")
-def download():
-    return send_file("static/qrcode.png", as_attachment=True)
+    return render_template("index.html", qr=qr)
 
 if __name__ == "__main__":
     app.run(debug=True)
